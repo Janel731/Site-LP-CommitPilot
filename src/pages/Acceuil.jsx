dededccd4ba228772019dd3bd41a1d44acbcd932
Janel <gratiasdeo418@gmail.com>
2026-06-16 22:32:01 +0100

    feat: ajout de composants Hero et Navbar dans la page d'accueil

--- src/pages/Acceuil.jsx
+++ src/pages/Acceuil.jsx
@@ -1,9 +1,11 @@
+import Hero from '@/components/Hero'
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 export const Acceuil = () => {
   return (
     <>
-        
+       <Hero></Hero>
     </>
   )
 }
