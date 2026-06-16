f3ac094b4d412d3009469eac41bae68f21a2645d
Janel <gratiasdeo418@gmail.com>
2026-06-16 23:08:30 +0100

    feat: mise à jour du titre de la barre de navigation de "SOLUTIONS" à "Acceuil"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import {
 } from "lucide-react";
 
 const navigationItems = [
-  { title: "SOLUTIONS", href: "#" },
+  { title: "Acceuil", href: "#" },
   { title: "INDUSTRIES", href: "#" },
   { title: "RESOURCES", href: "#" },
   { title: "ABOUT US", href: "#" },
