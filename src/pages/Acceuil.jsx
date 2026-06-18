9d2e51a09c04da4a581dc1f5efbbcf181338551c
Janel <gratiasdeo418@gmail.com>
2026-06-18 22:03:43 +0100

    ajout de pied de page dans la page d'accueil

--- src/pages/Acceuil.jsx
+++ src/pages/Acceuil.jsx
@@ -19,6 +19,7 @@ import React from 'react'
       <DocumentationPreview></DocumentationPreview>
       <ReviewsCTA></ReviewsCTA>
       <FinalCTA></FinalCTA>
+      <Footer></Footer>
     </>
   )
 }
