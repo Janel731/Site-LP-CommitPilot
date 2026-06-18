8f6e363a652327c1e1fed7e1f3cababbacdb8077
Janel <gratiasdeo418@gmail.com>
2026-06-18 22:02:43 +0100

    fix: remplacement de l'icône Linkedin par FaLinkedin dans le composant Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -66,7 +66,7 @@ export default function Footer() {
               href="#"
               className="rounded-full border border-zinc-800 p-3 text-zinc-400 transition hover:border-blue-500 hover:text-blue-400"
             >
-              <Linkedin size={18} />
+              <FaLinkedin size={18} />
             </a>
 
           </div>
